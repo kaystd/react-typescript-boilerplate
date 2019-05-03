75f66b4c84f528de4d7732199015444d0f9c8476
Danil <kaystd@gmail.com>
2019-05-03 18:31:25 +0700

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    open: 'google-chrome',
+    host: 'localhost',
+    port: 3000,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+    progress: true,
+  },
 }
